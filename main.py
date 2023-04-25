3604349df9a3019f449a41daa2a27e6119155d78
compile-run-Ali <91612802+compile-run-Ali@users.noreply.github.com>
2023-04-25 19:49:20 +0500

    settings

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from config import BOT_TOKEN
 import telebot # pip install pyTelegramBotAPI
 from components.start import start #import the start function from the start.py file
 from components.register import register #import the register function from the register.py file
+from components.settings import settings #import the settings function from the settings.py file
 from components.database import DB
 
 mint_bot = telebot.TeleBot(BOT_TOKEN, parse_mode=None) # create a bot object with the bot token we have
@@ -9,6 +10,7 @@ mint_bot = telebot.TeleBot(BOT_TOKEN, parse_mode=None) # create a bot object wit
 #message handler for the /start command
 mint_bot.register_message_handler(start, pass_bot=True, commands=['start'])
 mint_bot.register_message_handler(register, pass_bot=True, commands=['register'])
+mint_bot.register_message_handler(settings, pass_bot=True, commands=['settings'])
 
 me = mint_bot.get_me() #get the bot information
 print(me.username) #print the bot username
