08f2a2c5e6ca4d91dc0ba51e0bfb329a5406de23
FahadZaheerfzr <fahadzaheer19@gmail.com>
2023-04-26 00:24:47 +0500

    Added comments

--- listner.py
+++ listner.py
@@ -12,15 +12,25 @@ me = mint_bot.get_me() #get the bot information
 print(me.username) #print the bot username
 
 
-
 while True:
+    # Find all the groups
     groups = DB['group'].find()
+
+    # Loop through the groups
     for group in groups:
+        # Sleep for 1 second
         time.sleep(1)
+        # Get the group information
         url = group['url']
         chat_id = group['_id']
         contractId = group['contractId']
+        
+        # Get the last transaction count from the database if it exists else get the initial transaction count from the blockchain
         transactionCount = group['lastTransactionCount'] if 'lastTransactionCount' in group else getInitialTransactionCount(contractId) - 1
+
+        # Call the listener function
         transactionCount = listener(transactionCount, mint_bot, chat_id, url, contractId)
+
+        # Update the last transaction count in the database
         DB['group'].update_one({"_id": chat_id}, {"$set": {"lastTransactionCount": transactionCount}})
 
