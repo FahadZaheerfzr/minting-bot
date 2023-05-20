8c0d0381efd94d0d7cc26af932c6a67cfbf67869
FahadZaheerfzr <fahadzaheer19@gmail.com>
2023-05-21 01:05:57 +0500

    Name supported

--- components/listner/helper.py
+++ components/listner/helper.py
@@ -1,35 +1,34 @@
 import requests
-from config import API_KEY,RPC_ADDRESS, NFT_ABI
+from config import API_KEY, RPC_ADDRESS, NFT_ABI, ERC_ABI
 from web3 import Web3
 from datetime import datetime
 from components.database import DB
 from components.listner.networkConfig import NetworkConfig
 
 
-def getInitialTransactionCount(contractAddress:str, chat_id:int):
+def getInitialTransactionCount(contractAddress: str, chat_id: int):
     '''
     This function returns the initial transaction count
     Args:
         contractAddress (str): The contract address
-    
+
     Returns:
         int: The initial transaction count
     '''
-    
+
     # Parameters for the API call
     start_block = 0
     end_block = 999999999
 
-    #get the network from db
+    # get the network from db
     network = DB['group'].find_one({"_id": chat_id})['network']
 
-    #get the network config
+    # get the network config
     networkConfig = NetworkConfig(network)
 
-
-    
     # Make an API call to get the latest minted token
-    response = requests.get(f'{networkConfig.api_url}?module=account&action=txlist&address={contractAddress}&startblock={start_block}&endblock={end_block}&sort=asc&apikey=' + networkConfig.get_api_key())
+    response = requests.get(
+        f'{networkConfig.api_url}?module=account&action=txlist&address={contractAddress}&startblock={start_block}&endblock={end_block}&sort=asc&apikey=' + networkConfig.get_api_key())
 
     # Convert the response to JSON
     response = response.json()
@@ -41,58 +40,59 @@ def getInitialTransactionCount(contractAddress:str, chat_id:int):
     return data_length
 
 
-def getTokenInfo(tokenAddress,tokenId,chat_id):
+def getTokenInfo(tokenAddress, tokenId, chat_id):
     '''
     This function returns the token info
     Args:
         tokenAddress (str): The token address
         tokenId (int): The token id
-    
+
     Returns:
         dict: The token info
     '''
-    #get the network from db
+    # get the network from db
     network = DB['group'].find_one({"_id": chat_id})['network']
 
-    #get the network config
+    # get the network config
     networkConfig = NetworkConfig(network)
 
     # Create the web3 object
     web3 = Web3(Web3.HTTPProvider(networkConfig.rpc_url))
 
-
     # Create the contract object
     tokenContract = web3.eth.contract(address=tokenAddress, abi=NFT_ABI)
-    
+    erc_contract = web3.eth.contract(address=tokenAddress, abi=ERC_ABI)
+
+    name = erc_contract.functions.name().call()
     # Get the token info
     try:
         maxSupply = tokenContract.functions.maxSupply().call()
     except:
         maxSupply = "Infinity"
-    
 
     tokenURI = tokenContract.functions.tokenURI(tokenId).call()
     totalSupply = tokenContract.functions.totalSupply().call()
 
     # Return the token info
     return {
+        "name": name,
         "maxSupply": maxSupply,
         "tokenURI": tokenURI,
         "totalSupply": totalSupply
     }
 
 
-
 def getNFTs(froms, hashes, contractId, chat_id):
-    #get the network from db
+    # get the network from db
     network = DB['group'].find_one({"_id": chat_id})['network']
 
-    #get the network config
+    # get the network config
     networkConfig = NetworkConfig(network)
 
     nftsMinted = []
     for i in range(len(froms)):
-        transactions = requests.get(f'{networkConfig.api_url}?module=account&action=tokennfttx&contractaddress={contractId}&address={froms[i]}&page=1&offset=100&sort=asc&apikey={networkConfig.get_api_key()}')
+        transactions = requests.get(
+            f'{networkConfig.api_url}?module=account&action=tokennfttx&contractaddress={contractId}&address={froms[i]}&page=1&offset=100&sort=asc&apikey={networkConfig.get_api_key()}')
         transactions = transactions.json()
         if transactions['result']:  # check if 'result' is not empty
             for tx in sorted(transactions['result'], key=lambda x: x['timeStamp'], reverse=True):
@@ -109,10 +109,9 @@ def getNFTs(froms, hashes, contractId, chat_id):
     return nftsMinted
 
 
-
-def formattedPost(id, from_address, consumed, max, timestamp):
+def formattedPost(name, id, from_address, consumed, max, timestamp):
     return f"""
-    🟩 <b>SSSS #{id}</b> has been minted \n
+    🟩 <b>{name} #{id}</b> has been minted \n
 <code>Minter</code>: {from_address}\n
 <code>NFTs left</code>: <b> {consumed} / {max}</b>\n
 <code>Timestamp</code>: {timestamp} +UTC\n
--- components/listner/listenTransactions.py
+++ components/listner/listenTransactions.py
@@ -5,6 +5,7 @@ from components.listner.helper import getTokenInfo, formattedPost, getNFTs
 from components.listner.networkConfig import NetworkConfig
 from components.database import DB
 
+
 def listener(transactionCount, bot, chat_id, url, contractId, methodId, lastTokenID):
     # get the network from db
     network = DB['group'].find_one({"_id": chat_id})['network']
@@ -13,30 +14,32 @@ def listener(transactionCount, bot, chat_id, url, contractId, methodId, lastToke
     networkConfig = NetworkConfig(network)
 
     # Make an API call to get the latest minted token
-    response = requests.get(f'{networkConfig.api_url}?module=account&action=txlist&address={contractId}&startblock=0&endblock=999999999&sort=asc&apikey=' + networkConfig.get_api_key())
+    response = requests.get(
+        f'{networkConfig.api_url}?module=account&action=txlist&address={contractId}&startblock=0&endblock=999999999&sort=asc&apikey=' + networkConfig.get_api_key())
 
     # Convert the response to JSON
     response = response.json()
 
-    data = sorted(response['result'], key=lambda x: x['timeStamp'], reverse=True)
+    data = sorted(response['result'],
+                  key=lambda x: x['timeStamp'], reverse=True)
     data_length = len(data)
     if data_length <= transactionCount:
         if data_length == 10000:
             return (data_length - 100, None)
         return (data_length, None)
-    
+
     # Get hashes of all transactions
     hashes = []
     for i in range(data_length-transactionCount):
         if data[i]['methodId'] == methodId:
             hashes.append(data[i]['hash'])
-    
+
     # Get the from addresses of all transactions
     froms = []
     for i in range(data_length-transactionCount):
         if data[i]['from'] not in froms and data[i]['methodId'] == methodId:
             froms.append(data[i]['from'])
-        
+
     # To get the token ids
     nftsMinted = getNFTs(froms, hashes, contractId, chat_id)
     for nft in nftsMinted:
@@ -47,7 +50,7 @@ def listener(transactionCount, bot, chat_id, url, contractId, methodId, lastToke
         except Exception as e:
             print(e)
             continue
-        
+
         try:
             image = requests.get(tokenInfo['tokenURI']).json()['image']
         except:
@@ -55,21 +58,26 @@ def listener(transactionCount, bot, chat_id, url, contractId, methodId, lastToke
 
         maxSupply = tokenInfo['maxSupply']
         totalSupply = tokenInfo['totalSupply']
+        name = tokenInfo['name']
 
-        # Create the "Mint here!" 
-        button = types.InlineKeyboardButton(text="Mint here!", callback_data="mint", url=url)
+        # Create the "Mint here!"
+        button = types.InlineKeyboardButton(
+            text="Mint here!", callback_data="mint", url=url)
         markup = types.InlineKeyboardMarkup()
         markup.add(button)
 
         # Create the formatted message
         if maxSupply == "Infinity":
-            caption = formattedPost(nft["id"], nft["from"], totalSupply, "Infinity", nft["timestamp"])
+            caption = formattedPost(
+                name, nft["id"], nft["from"], totalSupply, "Infinity", nft["timestamp"])
         else:
-            caption = formattedPost(nft["id"], nft["from"], maxSupply-totalSupply, maxSupply, nft["timestamp"])
+            caption = formattedPost(
+                name, nft["id"], nft["from"], maxSupply-totalSupply, maxSupply, nft["timestamp"])
 
         # Send the message with the image and button, and the inline keyboard with the "Mint here!" button
         try:
-            bot.send_photo(chat_id=f"{chat_id}", photo=image, caption=caption, reply_markup=markup, parse_mode='HTML')
+            bot.send_photo(chat_id=f"{chat_id}", photo=image,
+                           caption=caption, reply_markup=markup, parse_mode='HTML')
         except Exception as e:
             print(e)
 
