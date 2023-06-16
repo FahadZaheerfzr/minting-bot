c3dfa0dab069386adab57b13be9aea0ab5fc3dc3
compile-run-Ali <91612802+compile-run-Ali@users.noreply.github.com>
2023-06-16 12:56:58 +0500

    monitoring script (start this instead of listner)

---
+++ monitor.py
@@ -0,0 +1,19 @@
+import subprocess
+import time
+
+def run_script():
+    while True:
+        # Start the script as a subprocess
+        script_process = subprocess.Popen(['python', 'listner.py'])
+        
+        # Wait for the script to finish
+        script_process.wait()
+        
+        # If the script exited normally (return code 0), break the loop
+        if script_process.returncode == 0:
+            break
+        
+        print("Script stopped. Restarting...")
+        time.sleep(5)  # Wait for 5 seconds before restarting
+
+run_script()
